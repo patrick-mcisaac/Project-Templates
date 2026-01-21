cbb7ab659eee2b8f104b61fc32f648fc113f8550
Patrick-McIsaac <patrick.mcisaac.dev@gmail.com>
2026-01-21 14:56:07 -0600

    no need to add custom gitignore

--- nextjs-tailwind-win.sh
+++ nextjs-tailwind-win.sh
@@ -187,53 +187,6 @@ EOF
 
 # git init
 
-cat > .gitignore << 'EOF'
-# See https://help.github.com/articles/ignoring-files/ for more about ignoring files.
-
-# dependencies
-/node_modules
-/.pnp
-.pnp.*
-.yarn/*
-!.yarn/patches
-!.yarn/plugins
-!.yarn/releases
-!.yarn/versions
-
-# testing
-/coverage
-
-# next.js
-/.next/
-/out/
-
-# production
-/build
-
-# misc
-.DS_Store
-*.pem
-
-# debug
-npm-debug.log*
-yarn-debug.log*
-yarn-error.log*
-.pnpm-debug.log*
-
-# env files (can opt-in for committing if needed)
-.env*
-
-# vercel
-.vercel
-
-# typescript
-*.tsbuildinfo
-next-env.d.ts
-
-#vs code
-/.vscode/
-EOF
-
 # git remote add origin "${Repo_Name}"
 # git branch -M main
 
@@ -241,5 +194,6 @@ EOF
 # git commit -m 'initial commit'
 # git push -u origin main
 
+echo "copy and paste cd ~/workspace/${Project_Name}-client"
 
 
